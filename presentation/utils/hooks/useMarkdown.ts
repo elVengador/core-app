938ce268635004fa1bac0f6c9b1cd4125b558c0b
elVengador <jimynicanor1001@gmail.com>
2022-04-07 20:46:03 -0500

    enable options to extended markdown

--- presentation/utils/hooks/useMarkdown.ts
+++ presentation/utils/hooks/useMarkdown.ts
@@ -1,9 +1,18 @@
-import { Converter } from 'showdown'
+import showdown from 'showdown'
 
 export const useMarkdown = (): { markdownToHtml: (markdown: string) => string } => {
 
     const markdownToHtml = (markdown: string) => {
-        const converter = new Converter()
+        const converter = new showdown.Converter({
+            tables: true,                           // enable tables
+            strikethrough: true,                    // enable strikethrough
+            ghCodeBlocks: true,                     // enable code blocks
+            tasklists: true,                        // enable task list
+            simpleLineBreaks: true,                 // enable real breaks lines
+            ghCompatibleHeaderId: true,             // add - instead space in ids
+            openLinksInNewWindow: true
+        });
+
         return converter.makeHtml(markdown)
     }
 
