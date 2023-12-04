8fc00841daa7ff8a996ef73633acda264d27b162
elVengador <dev.jimynicanor@gmail.com>
2023-12-03 20:02:17 -0500

    update as fas as iconPack

--- presentation/utils/font-awesome.util.ts
+++ presentation/utils/font-awesome.util.ts
@@ -1,5 +1,5 @@
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { IconPack, library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 
-library.add(fas)
+library.add(fas as IconPack)
 
