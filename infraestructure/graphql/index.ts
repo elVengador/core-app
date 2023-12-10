e008119b8d35c7b6afc22024f57b55f229e93669
elVengador <dev.jimynicanor@gmail.com>
2023-12-10 00:05:05 -0500

    update endpoint protocol

--- infraestructure/graphql/index.ts
+++ infraestructure/graphql/index.ts
@@ -1,5 +1,6 @@
 import { GraphQLClient } from 'graphql-request';
 import { API_HOST, API_PORT } from '../../core.enviroments';
 
-const endpoint = `http://${API_HOST}:${API_PORT}/graphql`
+const PROTOCOL = process.env.NODE_ENV === 'development'? "http":"https"
+const endpoint = `${PROTOCOL}://${API_HOST}:${API_PORT}/graphql`
 export const gqlClient = new GraphQLClient(endpoint, { headers: {} })
